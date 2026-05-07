67c266849c566ee1d1583a2568a2cc69aa3c5b2f
gabopoopam <gabriel.figueroa2027@adoc.superate.org.sv>
2026-05-06 18:40:15 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "tidesurf";
+
+    $conn = new mysqli($host, $user, $password, $dbname);
+
+     if ($conn->connect_error) {
+         die("Error de conexión: " . $conn->connect_error);
+    }
+
+    $conn->set_charset("utf8mb4");
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>TideSurf Registro</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+  
+</head>
+<body>
+
+<div class="card">
+    <img src="img/logo.jpg" alt="logo">
+  <h3>Registro</h3>
+
+  <h1>Únete a TideSurf</h1>
+  <p>y conecta con el océano</p>
+
+  <div class="input-box">
+    <i class="fa fa-user"></i>
+    <input type="text" placeholder="Nombre completo">
+  </div>
+
+  <div class="input-box">
+    <i class="fa fa-envelope"></i>
+    <input type="email" placeholder="Correo Electrónico">
+  </div>
+
+  <div class="input-box">
+    <i class="fa fa-lock"></i>
+    <input type="password" placeholder="Contraseña">
+    <i class="fa fa-eye-slash eye"></i>
+  </div>
+
+  <div class="input-box">
+    <i class="fa fa-lock"></i>
+    <input type="password" placeholder="Confirmar Contraseña">
+    <i class="fa fa-eye-slash eye"></i>
+  </div>
+
+  <div class="extra">
+    <span>¿Tienes cuenta?</span>
+    <a href="#">Iniciar Sesión</a>
+  </div>
+
+  <button>Registrarse</button>
+</div>
+
+</body>
+</html>
